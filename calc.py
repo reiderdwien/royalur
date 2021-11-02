995f097798c5eab3fdc9a6499b9fe6d556090cdb
Reid Erdwien <rderdwien@gmail.com>
2021-11-01 21:27:10 -0600

    playing with bounds.  The only needed one seems to be the min on p1

--- calc.py
+++ calc.py
@@ -6,9 +6,9 @@ n = 60
 
 def calc(p,m,n):
     states = 0
-    for p1 in range(0,min(m+n+1,p+1)):
-        for p2 in range(0,min(m+n+1,p+1)):
-            for j in range(max(0,p1-m), min(n,p1) + 1):
+    for p1 in range(0,min(m+n+1, p+1)):
+        for p2 in range(0,min(m+n+1, p+1)):
+            for j in range(0, p1 + 1):
                 add_states = (p-p1+1)*(p-p2+1)*math.comb(n, j)*math.comb(m, p1 - j)*math.comb(m+n-(p1-j), p2)
                 states = states + add_states
     return states
