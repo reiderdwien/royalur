987081e51e2afd55d7a641fbd1bc474032d8c102
Reid Erdwien <rderdwien@gmail.com>
2021-11-02 00:59:24 -0600

    considerations

--- calc.py
+++ calc.py
@@ -1,8 +1,9 @@
 import math
+import time
 
-p = 70
-m = 80
-n = 60
+p = 14
+m = 16
+n = 12
 
 def calc(p,m,n):
     states = 0
@@ -12,6 +13,17 @@ def calc(p,m,n):
                 add_states = (p-p1+1)*(p-p2+1)*math.comb(n, j)*math.comb(m, p1 - j)*math.comb(m+n-(p1-j), p2)
                 states = states + add_states
     return states
+
+def considerations(p,m,n,states):
+    delta_shared = 0
+    if p >= 4:
+        for p1 in range(4,min(m+n+1,p+1)):
+            delta_shared = delta_shared + 2*(p-p1+1)*math.comb(n+m-4,p1-4)
+    delta_turns = 0
+    for p1 in range(0,min(m+n+1,p+1)):
+        delta_turns = delta_turns + 2*(p-p1+1)*math.comb(n+m,p1)
+    return 2*(states) - delta_turns - delta_shared
+    # delta_turns already gets rid of the "both victory" scenario, since that is really one state, but the factor of 2 considers it as two states
 
 def tests():
     m = 8
@@ -41,4 +53,9 @@ def tests():
 
 tests()
 
+start = time.time()
 print(calc(p,m,n))
+end = time.time()
+print("The time of execution of above program is :", end-start)
+
+print(considerations(1,8,6,calc(1,8,6)))
